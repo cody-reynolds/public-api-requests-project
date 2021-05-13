1e6f99c233b8bd8e4cbb51e2aed47a8863e671fe
Cody Reynolds <codyreynolds@hey.com>
2021-05-13 17:43:39 -0400

    Make modal open/close functional

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,22 @@
 // ------------------------------------------
 //  VARIABLES
 // ------------------------------------------
-const randomEmployeesUrl = 'https://randomuser.me/api/?results=12&nat=us&inc=picture,name,email,location,phone,dob';
+const randomUsersUrl = 'https://randomuser.me/api/?results=12&nat=us&inc=picture,name,email,location,phone,dob';
 const searchContainer = document.querySelector('.search-container');
 const gallery = document.getElementById('gallery');
 const body = document.querySelector('body');
-const modal = document.querySelector('.modal');
 
 
 // ------------------------------------------
 //  FETCH FUNCTION
 // ------------------------------------------
 
-fetch(randomEmployeesUrl)
+    fetch(randomUsersUrl)
     .then(response => response.json())
     .then(data => {
         generateCards(data.results);
     })
+
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
@@ -30,7 +30,7 @@ function generatePerson(person) {
     let personCard = `
     <div class="card">
         <div class="card-img-container">
-            <img class="card-img" src="${person.picture.medium}" alt="profile picture">
+            <img class="card-img" src="${person.picture.large}" alt="profile picture">
         </div>
         <div class="card-info-container">
             <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
@@ -40,39 +40,36 @@ function generatePerson(person) {
     </div>`
     gallery.insertAdjacentHTML("beforeend", personCard);
 
-    //Assigning 'card' to the most recently added person's card.
+    
+    const modalCard = `
+    <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${person.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+                <p class="modal-text">${person.email}</p>
+                <p class="modal-text cap">${person.location.city}</p>
+                <hr>
+                <p class="modal-text">${person.phone}</p>
+                <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state} ${person.location.postcode}</p>
+                <p class="modal-text">Birthday: ${person.dob.date}</p>
+            </div>
+        </div>
+    </div> 
+    `
+
     let card = document.querySelector('.card:last-child');
 
+
     card.addEventListener("click", event => {
-        let modalCard = `
-        <div class="modal-container">
-            <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                <div class="modal-info-container">
-                    <img class="modal-img" src="${person.picture.medium}" alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
-                    <p class="modal-text">${person.email}</p>
-                    <p class="modal-text cap">${person.location.city}</p>
-                    <hr>
-                    <p class="modal-text">${person.phone}</p>
-                    <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state} ${person.location.postcode}</p>
-                    <p class="modal-text">Birthday: ${person.dob.date}</p>
-                </div>
-            </div>
-        </div> 
-        `
+        body.insertAdjacentHTML('beforeend',modalCard);
 
-        body.insertAdjacentHTML('beforeend', modalCard);
-
-        let modal = document.querySelector('.modal-container');
-
-        modal.addEventListener('click', (event) => {
-            console.log(event.target);
+        document.querySelector('.modal-container').addEventListener('click', (event) =>{
             if(event.target.textContent === 'X'){
-                modal.style.display = 'none';
+                document.querySelector('.modal-container').remove();
             }
-        })
-
+        });
 
     });
 }
